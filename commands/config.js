a9a3d8d34d1b7e1f0c98d6ac71441376ba64078e
shadowolf <shadowolfbusiness@gmail.com>
2021-11-18 22:05:30 +1100

    Moved to mysql and started db work.

--- commands/config.js
+++ commands/config.js
@@ -1,4 +1,6 @@
 const { Discord, MessageEmbed } = require('discord.js');
+const config = require('../config.json');
+const database = require('../databaseController');
 
 module.exports = {
     name: 'config',
@@ -8,6 +10,11 @@ module.exports = {
     testOnly: true,
 
     options: [
+        {
+            "name": "show",
+            "description": "Show the current configuration.",
+            "type": 1
+        },
         {
             "name": "setbotrole",
             "description": "Set the role for which role can configure the bot.",
@@ -49,7 +56,36 @@ module.exports = {
         },
     ],
     
-    callback: ({ client, interaction }) => {
+    callback: ({ interaction }) => {
+        // 
+        // /config show
+        // 
+        if (interaction.options.getSubcommand() === "show") {
+            try {
+                database.query (`SELECT * FROM config WHERE guildID=?;`, [interaction.guildId], function (error, results, fields) {
+                    if (error) {
+                      throw error;
+                    } else {
+                        console.log(results);
+                        const embed = new MessageEmbed()
+                        .setTitle(`${interaction.guild.name}'s Configuration`)
+                        .setColor(`${config.colours.info}`)
+                        .setDescription(`botRole: ${results[0].botRole}\nprayerRequestChannel: ${results[0].prayerRequestChannel}\nprayerRequestLogChannel: ${results[0].prayerRequestLogChannel}`)
+            
+                        interaction.reply({
+                            embeds: [embed],
+                            ephemeral: true 
+                        });
+                        return
+                    }
+                });
+                return
+            } catch (error) {
+                console.log(error);
+                return   
+            }
+        }
+
 
         // 
         // /config setbotrole
@@ -59,16 +95,22 @@ module.exports = {
             const botRoleDisplayName = interaction.options.getRole('role').name;
 
             try {
-                const embed = new MessageEmbed()
-                .setTitle(`The Bot Role has been set.`)
-                // .setColor(``)
-                .setDescription(`The bot role to control ArrowPrayer has been set to \`${botRoleDisplayName}\``)
-    
-                interaction.reply({
-                    embeds: [embed],
-                    ephemeral: true 
+                database.query (`UPDATE config SET botRole=? WHERE guildID=?;`, [botRoleID, interaction.guildId], function (error, results, fields) {
+                    if (error) {
+                      throw error;
+                    } else {
+                        const embed = new MessageEmbed()
+                        .setTitle(`The Bot Role has been set.`)
+                        .setColor(`${config.colours.success}`)
+                        .setDescription(`The bot role to control ArrowPrayer has been set to \`${botRoleDisplayName}\``)
+            
+                        interaction.reply({
+                            embeds: [embed],
+                            ephemeral: true 
+                        });
+                        return
+                    }
                 });
-                return
             } catch (error) {
                 console.log(error);
                 return   
@@ -84,10 +126,41 @@ module.exports = {
             const requestChannelDisplayName = interaction.options.getChannel('requestchannel').name;
 
             try {
+                database.query (`UPDATE config SET botRole=? WHERE guildID=?;`, [botRoleID, interaction.guildId], function (error, results, fields) {
+                    if (error) {
+                      throw error;
+                    } else {
+                        const embed = new MessageEmbed()
+                        .setTitle(`The Prayer Request channel has been set.`)
+                        .setColor(`${config.colours.success}`)
+                        .setDescription(`The channel where requests will go has been set to \`${requestChannelDisplayName}\``)
+            
+                        interaction.reply({
+                            embeds: [embed],
+                            ephemeral: true 
+                        });
+                        return
+                    }
+                });
+            } catch (error) {
+                console.log(error);
+                return   
+            }
+        }
+
+
+        // 
+        // /config setprayerrequestlogchannel
+        // 
+        if (interaction.options.getSubcommand() === "setprayerrequestlogchannel") {
+            const requestLogChannelID = interaction.options.getChannel('logchannel').id;
+            const requestLogChannelDisplayName = interaction.options.getChannel('logchannel').name;
+
+            try {
                 const embed = new MessageEmbed()
-                .setTitle(`The Prayer Request channel has been set.`)
-                // .setColor(``)
-                .setDescription(`The channel where requests will go has been set to \`${requestChannelDisplayName}\``)
+                .setTitle(`The Prayer Request Log channel has been set.`)
+                .setColor(`${config.colours.success}`)
+                .setDescription(`The channel where requests will be logged has been set to \`${requestLogChannelDisplayName}\``)
     
                 interaction.reply({
                     embeds: [embed],
